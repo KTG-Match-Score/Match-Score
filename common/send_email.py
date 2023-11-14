36694feaed0c33e42757bbca11ef636e7014d191
GeorgiTashev <g.tashev@gmail.com>
2023-11-14 13:17:41 +0200

    full_registration

---
+++ common/send_email.py
@@ -0,0 +1,31 @@
+from mailjet_rest import Client
+import os
+
+def send_email( 
+    recipient_email: str, 
+    recipient_name:str, 
+    validation_code: str|None = None):
+    api_key = os.environ['MAILJET_API_KEY']
+    api_secret = os.environ['MAILJET_SECRET_KEY']
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+    if validation_code:
+        data = {
+            'Messages': [
+                {
+                    "To": [
+                        {
+                            "Email": recipient_email,
+                            "Name": recipient_name
+                        }
+                    ],
+                    "TemplateID": 5302951,
+                    "TemplateLanguage": True,
+                    "Variables": {
+                        "name": recipient_name,
+                        "validation_code": validation_code
+                    }
+                }
+            ]
+        }
+        response = mailjet.send.create(data=data)
+        return response
--- services/users_services.py
+++ services/users_services.py
@@ -1,4 +1,4 @@
-from data.database import insert_query, update_query
+from data.database import insert_query, update_query, read_query
 from models.user import User
 from datetime import datetime
 from mariadb import IntegrityError
@@ -20,7 +20,18 @@ def register (email:str, password: str, fullname:str):
                      (email, hashed_password,fullname, role, db_picture))
         return email, password
     except IntegrityError as err:
-        return 
+        raise IntegrityError 
+
+def validate(validation_code, email):
+    db_validation_code = read_query('''select validation_code from users where email=?''',(email,))
+    if db_validation_code[0][0] == validation_code:
+        return True
+    else:
+        return
+    
+
+def update_validation_code(user_id: int, validation_code: str):
+    return update_query(''' update users set validation_code =? where id = ? ''', (validation_code, user_id))
     
 def set_admin(id: int):  
     return update_query("update users set is_admin = ? where id_user =?",(1, id))
