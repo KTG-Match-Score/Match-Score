ec8666f52bd41aa4505f69f480fa812a7e87d336
GeorgiTashev <g.tashev@gmail.com>
2023-11-14 16:55:30 +0200

    implemented login

--- services/users_services.py
+++ services/users_services.py
@@ -6,7 +6,7 @@ import common.auth as auth
 import common.picture_handler as ph
 
 
-def register (email:str, password: str, fullname:str):
+async def register (email:str, password: str, fullname:str):
     try:
         auth.check_password(password)
     except AssertionError as error:
@@ -22,7 +22,7 @@ def register (email:str, password: str, fullname:str):
     except IntegrityError as err:
         raise IntegrityError 
 
-def validate(validation_code, email):
+async def validate(validation_code, email):
     db_validation_code = read_query('''select validation_code from users where email=?''',(email,))
     if db_validation_code[0][0] == validation_code:
         return True
@@ -30,11 +30,28 @@ def validate(validation_code, email):
         return
     
 
-def update_validation_code(user_id: int, validation_code: str):
+async def update_validation_code(user_id: int, validation_code: str):
     return update_query(''' update users set validation_code =? where id = ? ''', (validation_code, user_id))
     
-def set_admin(id: int):  
-    return update_query("update users set is_admin = ? where id_user =?",(1, id))
+
+async def reset_password(id: int):
+    reset_password = auth.generate_six_digit_code()
+    hashed_password = auth.get_password_hash(reset_password)
+    update_query('''update users set password =?, validated_password = ? where id = ?''',(hashed_password, 0, id))
+    return reset_password
+
+async def update_password(id: int, password: str):
+    hashed_password = auth.get_password_hash(password)
+    update_query('''update users set password =?, validated_password = ? where id = ?''',(hashed_password, 1, id))
+
+async def check_validated_account(id: int):
+    db_validated = read_query('''select is_validated, validated password from users where email=?''',(id,))
+    return db_validated 
+    
+    
+    
+    
+    
 
 
 
