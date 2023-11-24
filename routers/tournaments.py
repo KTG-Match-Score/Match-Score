b2d07cc34e153e7638caec7b9be72599eef3219d
“Kaloyan <kokoeverest@gmail.com>
2023-11-24 12:04:05 +0200

    refactor: connecting tournaments and matches

--- routers/tournaments.py
+++ routers/tournaments.py
@@ -1,15 +1,13 @@
-from fastapi import APIRouter, status, Form, Request, Query
+from fastapi import APIRouter, status, Form, Request, Path, Query
 from models.tournament import Tournament, MatchesInTournament
+from models.player import Player
 import routers.players as players
 import common.auth as auth
-from typing import Optional
+from typing import Annotated, Optional
 from services import tournaments_services
 from fastapi.templating import Jinja2Templates
 from datetime import date, datetime
-from fastapi.responses import RedirectResponse, JSONResponse
-import json
-import httpx
-
+from fastapi.responses import RedirectResponse
 
 
 tournaments_router = APIRouter(prefix="/tournaments")
@@ -23,39 +21,15 @@ async def view_tournaments(request: Request,
 
     tournaments = tournaments_services.get_tournaments(sport_name, tournament_name)
     
-    return tournaments
-
-    # return templates.TemplateResponse("return_tournaments.html", context={"request": request, "tournaments": tournaments})
+    return templates.TemplateResponse("return_tournaments.html", context={"request": request, "tournaments": tournaments})
 
 @tournaments_router.get("/create_tournament_form")
 async def show_create_tournament_form(request: Request):
-    access_token = request.cookies.get("access_token")
-    refresh_token = request.cookies.get("refresh_token")
-    tokens = {"access_token": access_token, "refresh_token": refresh_token}
-    try:
-        user = await auth.get_current_user(access_token)
-    except:
-        try:
-            user = auth.refresh_access_token(access_token, refresh_token)
-            tokens = auth.token_response(user)
-        except:
-            RedirectResponse(url='/landing_page', status_code=303)
+    return templates.TemplateResponse("create_tournament_form.html", context={"request": request})
 
-    if user.role != "director":
-        RedirectResponse(url='/landing_page', status_code=303)
-
-    response = templates.TemplateResponse("create_tournament_form.html", context={"request": request})
-
-    response.set_cookie(key="access_token",
-                        value=tokens["access_token"], httponly=True)
-    response.set_cookie(key="refresh_token",
-                        value=tokens["refresh_token"], httponly=True)
-    
-    return response
-
-# @tournaments_router.get("/add_players")
-# async def add_players_to_tournament(request: Request):
-#     return 
+@tournaments_router.get("/add_players")
+async def add_players_to_tournament(request: Request):
+    return 
 
 @tournaments_router.get("/{date}", status_code=status.HTTP_200_OK, response_model=list[MatchesInTournament])
 async def view_tournaments_by_date(request: Request,
@@ -63,11 +37,8 @@ async def view_tournaments_by_date(request: Request,
     tournaments_matches = tournaments_services.get_tournaments_by_date(date)
 
     if tournaments_matches == {}:
-        return JSONResponse(content="No events for the selected date.")
-    return tournaments_matches
-    # if tournaments_matches == {}:
-    #     return templates.TemplateResponse("return_no_tournaments_by_date.html", context={"request": request})
-    # return templates.TemplateResponse("return_tournaments_by_date.html", context={"request": request, "tournaments_matches": tournaments_matches})
+        return templates.TemplateResponse("return_no_tournaments_by_date.html", context={"request": request})
+    return templates.TemplateResponse("return_tournaments_by_date.html", context={"request": request, "tournaments_matches": tournaments_matches})
 
 @tournaments_router.post("/create_tournament")
 async def create_tournament(request: Request,
@@ -122,19 +93,13 @@ async def create_tournament(request: Request,
     return response
 
 @tournaments_router.post("/create_tournament_schema")
-async def create_tournament_schema(request: Request):
-    
-    json_data = await request.json()
-
-    tournament_id = json_data.get("tournament_id")
-    participants_per_match = json_data.get("participants_per_match")
-    format = json_data.get("format")
-    number_participants = json_data.get("number_participants")
-    sport = json_data.get("sport")
+async def create_tournament_schema(request: Request,
+                                   data: tuple[Tournament, int, str]):
+    tournament, participants, sport = data
 
     access_token = request.cookies.get("access_token")
     refresh_token = request.cookies.get("refresh_token")
-    
+    tokens = {"access_token": access_token, "refresh_token": refresh_token}
     try:
         user = await auth.get_current_user(access_token)
     except:
@@ -147,10 +112,12 @@ async def create_tournament_schema(request: Request):
     if user.role != "director":
         RedirectResponse(url='/landing_page', status_code=303)
 
-    schema = tournaments_services.generate_schema(tournament_id, participants_per_match, format, number_participants, sport)
+    schema = tournaments_services.generate_schema(tournament, participants, sport)
 
-    cookies = request.cookies
-    async with httpx.AsyncClient() as client:
-        response = await client.post("http://localhost:8000/matches/create", cookies=cookies, json={ "tournament_id":tournament_id, "format":format, "schema": schema}) 
+    response =  ... # players.templates.TemplateResponse("waiting_for_template.html", {"request": request, "schema": schema, "tournament": tournament})
+    response.set_cookie(key="access_token",
+                        value=tokens["access_token"], httponly=True)
+    response.set_cookie(key="refresh_token",
+                        value=tokens["refresh_token"], httponly=True)
     
     return response
